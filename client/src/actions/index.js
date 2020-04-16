7eaf16cfc0128978e94945d315a2da2e31c866ac
Ngo Huy Hoang <huyhoang82@gmail.com>
2020-04-16 15:24:40 +0700

    add delete function

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -57,6 +57,8 @@ export const deleteStream = (id) => async dispatch => {
     await streams.delete(`/streams/${id}`);
 
     dispatch({ type: DELETE_STREAM, payload: id });
+
+    history.push('/');
 };
 
 
