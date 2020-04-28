ae25671fd7b62762b9f811070c02f8e398efcaca
Ngo Huy Hoang <huyhoang82@gmail.com>
2020-04-28 09:54:49 +0700

    using dotNet Backend

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -45,8 +45,10 @@ export const fetchStream = (id) => async dispatch => {
     dispatch({ type: FETCH_STREAM, payload: response.data })
 };
 
-export const editStream = (id, formValues) => async dispatch => {
-    const response = await streams.patch(`/streams/${id}`, formValues);
+export const editStream = (id, formValues) => async (dispatch, getState) => {
+    const { userId } = getState().auth;
+    
+    const response = await streams.put(`/streams/${id}`, { ...formValues, userId, id});
 
     dispatch({ type: EDIT_STREAM, payload: response.data });
 
